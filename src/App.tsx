48da22a3654ea051380a0cec70127290e2f8445b
unnamed1164 <14520618@gm.uit.edu.vn>
2020-06-08 07:27:50 +0700

    Update ver1

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 interface State{
-  apiResponse:string
+  apiResponse:string,
+  dbResponse:string
 }
 interface Props{
 }
 class App extends React.Component<Props,State> {
   constructor(props:Props) {
     super(props);
-    this.state = { apiResponse: "" };
+    this.state = {dbResponse:"",
+    apiResponse: "" };
   }
   callAPI() {
-    fetch("http://localhost:5000/testAPI")
+    fetch("http://localhost:9000/testAPI")
       .then(res => res.text())
       .then(res => this.setState({ apiResponse: res }));
   }
+  callDB() {
+    fetch("http://localhost:9000/testDB")
+        .then(res => res.text())
+        .then(res => this.setState({ dbResponse: res }))
+        // .catch(err => err);
+}
   componentDidMount() {
     this.callAPI();
+    this.callDB();
   }
   render() {
     console.log(this.state)
@@ -25,7 +34,9 @@ class App extends React.Component<Props,State> {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p className="App-intro">{this.state.apiResponse}</p>
+          {/* <p className="App-intro">{this.state.apiResponse}</p> */}
+          <p className="App-intro">{this.state.dbResponse}</p>
+
         </header>
       </div>
     );
